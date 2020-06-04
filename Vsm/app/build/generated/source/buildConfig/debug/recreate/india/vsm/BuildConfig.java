49355b1b05d3eed973901656571889fcb62f584d
Anubhav <18ucs066@lnmiit.ac.in>
2020-06-04 18:42:09 +0530

    new project

---
+++ Vsm/app/build/generated/source/buildConfig/debug/recreate/india/vsm/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package recreate.india.vsm;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "recreate.india.vsm";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ Vsm/app/src/main/java/recreate/india/vsm/MainActivity.java
@@ -0,0 +1,15 @@
+package recreate.india.vsm;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
